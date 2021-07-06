1b1a884fb081a2da3a443b62b1d1d629b5a0edfe
OllieWShouldBeCoding <owithington@hotmail.co.uk>
2021-07-06 11:56:36 +0100

    Basic Block Spawning Functionality
    Added two basic scripts and a prefab
    
    CityManager, which i imagine as the overall game manager script
    - Currently it just contains a hand coded test matrix, and it calls the spawn method on start
    
    script_BlockSpawner - which has a spawnBlocks method which takes in a matrix

---
+++ Assets/Scripts/CityManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityManager : MonoBehaviour
+{
+    public script_BlockSpawner blockSpawner;
+
+
+    private int[, ,] testMatrix = new int[,,] {{{1, 0, 1 , 1}, { 1, 0, 1 , 1} }, 
+                                 { { 0, 0, 1 , 1}, { 0, 0, 1 , 1},},
+                                 { { 0, 0, 1 , 1}, { 0, 0, 0 , 0},}};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        blockSpawner.spawnBlocks(testMatrix);
+    }
+
+}
