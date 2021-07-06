49d735321089c24497d7c2986db5eb5bd5b6ae12
OllieWShouldBeCoding <owithington@hotmail.co.uk>
2021-07-06 12:30:51 +0100

    Basic Roof Object

--- Assets/Scripts/CityManager.cs
+++ Assets/Scripts/CityManager.cs
@@ -7,9 +7,10 @@ public class CityManager : MonoBehaviour
     public script_BlockSpawner blockSpawner;
 
 
-    private int[, ,] testMatrix = new int[,,] {{{1, 0, 1 , 1}, { 1, 0, 1 , 1} }, 
-                                 { { 0, 0, 1 , 1}, { 0, 0, 1 , 1},},
-                                 { { 0, 0, 1 , 1}, { 0, 0, 0 , 0},}};
+    private int[, ,] testMatrix = new int[,,] {{{1, 0, 1 , 1}, { 1, 0, 1 , 1}, { 2, 0, 1 , 1} }, 
+                                 { { 0, 0, 1 , 1}, { 0, 0, 1 , 1},{ 0, 0, 1 , 1},},
+                                 { { 0, 0, 1 , 1}, { 0, 0, 0 , 0},{ 0, 0, 0 , 0},},
+                                 { { 0, 0, 1 , 1}, { 0, 0, 1 , 1},{ 0, 0, 2 , 2},}};
 
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/script_BlockSpawner.cs
+++ Assets/Scripts/script_BlockSpawner.cs
@@ -6,6 +6,8 @@ public class script_BlockSpawner : MonoBehaviour
 {
     public GameObject basicBlock;
 
+    public GameObject basicRoof;
+
 
     public void spawnBlocks(int[,,] inputMatrix){
 
@@ -16,12 +18,17 @@ public class script_BlockSpawner : MonoBehaviour
                 for (int z = 0; z<inputMatrix.GetLength(2); z++){
                     Debug.Log("Checking cell: " + x + "," + y + "," + z);
                     Debug.Log("Cell value: " + inputMatrix[x,y,z]);
+                    
+                    //Get spawn position
+                    Vector3 spawnPos = new Vector3(defaultSpawnPosition.x+x, defaultSpawnPosition.y+y, defaultSpawnPosition.z+z);
+
                     //If we find a 1 in our matrix, spawn a block at our position, offset by its matrix position
                     if (inputMatrix[x,y,z] == 1){
-                        
-                        //Get spawn position
-                        Vector3 spawnPos = new Vector3(defaultSpawnPosition.x+x, defaultSpawnPosition.y+y, defaultSpawnPosition.z+z);
                         Instantiate(basicBlock, spawnPos, this.transform.rotation, this.transform);
+                    }
+                    else if (inputMatrix[x,y,z] == 2){
+                        Instantiate(basicRoof, spawnPos, this.transform.rotation, this.transform);
+
                     }
 
                 }
